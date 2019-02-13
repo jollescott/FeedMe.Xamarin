69d78ec8bbe6079ce13d3b5546e444c63f41686f
JoLi0125@student.grillska.se <@JagGilarJulmust>
2019-02-13 14:18:38 +0100

    * FrameCornerEffectAndroid.cs: Fixed ios circular images
    * FeedMe.csproj:
    * FeedMe.iOS.csproj:
    * MealsListPage.xaml:
    * FrameCornerEffect.cs:
    * FeedMe.Android.csproj:
    * FrameCornerEffectIOS.cs:

---
+++ FeedMe/FeedMe.iOS/Effects/FrameCornerEffectIOS.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using FeedMe.Effects;
+using FeedMe.iOS.Effects;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ResolutionGroupName("FeedMe")]
+[assembly: ExportEffect(typeof(FrameCornerEffectIOS), "FrameCornerEffect")]
+namespace FeedMe.iOS.Effects
+{
+    public class FrameCornerEffectIOS : PlatformEffect
+    {
+
+        protected override void OnAttached()
+        {
+            var radius = (float)FrameCornerEffect.GetRadius(Element);
+            Container.Layer.CornerRadius = radius;
+            Container.Layer.MasksToBounds = true;
+        }
+
+        protected override void OnDetached()
+        {
+        }
+    }
+}
