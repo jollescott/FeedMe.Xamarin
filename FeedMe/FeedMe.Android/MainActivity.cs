3cd8d9c0c3b9767e0eb7da6a4faed8700adeebf1
Joel Linder <jollescott.carl@hotmail.com>
2020-06-11 16:44:39 +0200

    Admob fix

--- FeedMe/FeedMe.Android/MainActivity.cs
+++ FeedMe/FeedMe.Android/MainActivity.cs
@@ -20,16 +20,13 @@ namespace FeedMe.Droid
 
             base.OnCreate(savedInstanceState);
 
-            // FaceBook ads stuff
-            //AdSettings.AddTestDevice("4bc78db7-bd07-4bc2-9d11-1d1b19a5a415");
-            //AdSettings.AddTestDevice("c3d7b4ba-e06e-465b-9db3-1008974c7ebb");
-
             Rg.Plugins.Popup.Popup.Init(this, savedInstanceState);
 
             FFImageLoading.Forms.Platform.CachedImageRenderer.Init(false);
             Plugin.Iconize.Iconize.Init(Resource.Id.toolbar, Resource.Id.sliding_tabs);
 
-            AppCenter.Start("eedec111-2462-45bc-9c49-3320f6a175a3", typeof(Analytics), typeof(Crashes));
+            //TODO: Update to new version
+            //AppCenter.Start("eedec111-2462-45bc-9c49-3320f6a175a3", typeof(Analytics), typeof(Crashes));
 
             Forms.Init(this, savedInstanceState);
 
